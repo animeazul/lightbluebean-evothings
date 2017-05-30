e35d5ed26034e4caa914175a3891e684ab4ea634
animeazul <dajj@comunidad.unam.mx>
2017-05-30 02:40:55 -0500

    motor
    descripcion

--- LightBlueBean_Basic/LightBlueBean_Basic.ino
+++ LightBlueBean_Basic/LightBlueBean_Basic.ino
@@ -1,18 +1,34 @@
 
 String beanName = "LightBlueBean";
+int mot1i=9;
+int mot1d=8;
+int mot1e=7;
+int mot2i=6;
+int mot2d=4;
+int mot2e=5;
+int mot3i=1;
+int mot3d=0;
+int mot3e=2;
+
 const uint8_t ledScratch = 1;
 const uint8_t temperatureScratch = 2;
 const uint8_t BatteryScratch = 3;
 const uint8_t MotorScratch = 4;
 void setup() {
+   pinMode(mot1i, OUTPUT);
+   pinMode(mot1d, OUTPUT);
+   pinMode(mot2i, OUTPUT);
+   pinMode(mot2d, OUTPUT);
+   pinMode(mot3i, OUTPUT);
+   pinMode(mot3d, OUTPUT);
     // Setup bean
     Bean.setBeanName(beanName);
     Bean.enableWakeOnConnect(true);
     // Reset the scratch data area 1.
     uint8_t resetLedBuffer[] = {0, 0, 0};
     Bean.setScratchData(ledScratch, resetLedBuffer, 3);
-	uint8_t resetMotorBuffer[] = {0, 0, 0};
-	Bean.setScratchData(MotorScratch, resetMotorBuffer, 3);
+    uint8_t resetMotorBuffer[] = {0, 0, 0};
+    Bean.setScratchData(MotorScratch, resetMotorBuffer, 3);
 }
  
 // the loop routine runs over and over again forever:
@@ -28,7 +44,7 @@ void loop() {
 // Agregado ahorita
         uint8_t BatteryBuffer[1];
                 BatteryBuffer[0] = Bean.getBatteryLevel();
-        Bean.setScratchData(BatteryScatch, BatteryBuffer, 1);
+        Bean.setScratchData(BatteryScratch, BatteryBuffer, 1);
 // cerrado
 
         ScratchData receivedData = Bean.readScratchData(ledScratch);
@@ -38,11 +54,13 @@ void loop() {
         Bean.setLed(redLed, greenLed, blueLed);
 
 // Agregado ahorita
- 		ScratchData receivedData = Bean.readScratchData(MotorScratch);
-        uint8_t Motor1 = receivedData.data[0];
-        uint8_t Motor2 = receivedData.data[1];
-        uint8_t Motor3 = receivedData.data[2];
-        
+ 	ScratchData receivedData2 = Bean.readScratchData(MotorScratch);
+        uint8_t Motor1 = receivedData2.data[0];
+        uint8_t Motor2 = receivedData2.data[1];
+        uint8_t Motor3 = receivedData2.data[2];
+        analogWrite(mot1e, Motor1); 
+        digitalWrite(mot1i,LOW);
+        digitalWrite(mot1d,HIGH);
 // Cerrado    
 	}
     else {
